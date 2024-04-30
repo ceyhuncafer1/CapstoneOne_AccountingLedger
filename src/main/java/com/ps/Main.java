c90d31501f3e3b2f41784b927e8d4510213f9a7b
Ceyhun Cafer <ceyhuncafer1@gmail.com>
2024-04-30 15:07:55 -0400

    added home screen loop

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -1,7 +1,36 @@
 package com.ps;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        String choice;
+        do {
+            System.out.println("Home Screen:");
+            System.out.println("D) Add deposit");
+            System.out.println("P) Make payment (debit)");
+            System.out.println("L) Ledger");
+            System.out.println("X) Exit");
+            System.out.print("Choose an option: ");
+            choice = scanner.nextLine().toUpperCase();
+            switch (choice) {
+                case "D":
+
+                    break;
+                case "P":
+
+                    break;
+                case "L":
+
+                    break;
+                case "X":
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid option!");
+            }
+        } while (!choice.equals("X"));
     }
 }
