696ae50738311edf3fb8bb486ad826a2206a0a09
Ceyhun Cafer <ceyhuncafer1@gmail.com>
2024-05-01 04:14:15 -0400

    skeleton for reports

--- src/main/java/com/ps/Ledger.java
+++ src/main/java/com/ps/Ledger.java
@@ -11,7 +11,7 @@ public class Ledger {
     private ArrayList<Transaction> onlyDeposits;
     private ArrayList<Transaction> onlyPayments;
 
-    private static final DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    private static final DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("MM-dd-yyyy");
     private static final DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("HH:mm");
 
     public Ledger() {
@@ -25,7 +25,7 @@ public class Ledger {
     public void addDeposit(Scanner scanner) {
         try (PrintWriter writer = new PrintWriter(new FileWriter("transactions.txt", true))) {
 
-            System.out.print("Enter date (YYYY-MM-DD): ");
+            System.out.print("Enter date (MM-dd-yyyy): ");
             LocalDate date = LocalDate.parse(scanner.nextLine(), dateFormat);
             System.out.print("Enter time (HH:mm): ");
             LocalTime time = LocalTime.parse(scanner.nextLine(), timeFormat);
@@ -53,7 +53,7 @@ public class Ledger {
 
     public void makePayment(Scanner scanner) {
         try (PrintWriter writer = new PrintWriter(new FileWriter("transactions.txt", true))) {
-            System.out.print("Enter date (YYYY-MM-DD): ");
+            System.out.print("Enter date (MM-dd-yyyy): ");
             LocalDate date = LocalDate.parse(scanner.nextLine(), dateFormat);
             System.out.print("Enter time (HH:mm): ");
             LocalTime time = LocalTime.parse(scanner.nextLine(), timeFormat);
@@ -114,15 +114,56 @@ public class Ledger {
                 for (Transaction transaction : onlyPayments) {
                     System.out.println(transaction);
                 }
-                Collections.reverse(onlyDeposits);
+                Collections.reverse(onlyPayments);
                 break;
             case "R":
-                System.out.println("test3");
+
+                String nestedLedgerChoice = null;
+
+                do{
+
+                    System.out.println();
+                    System.out.println("Reports:");
+                    System.out.println();
+                    System.out.println("1) Month to Date");
+                    System.out.println("2) Previous Month");
+                    System.out.println("3) Year To Date");
+                    System.out.println("4) Previous Year");
+                    System.out.println("5) Search by Vendor");
+                    System.out.println("0) Back - Go back to the Report page");
+                    System.out.println();
+                    System.out.println("Choose an option");
+
+                    nestedLedgerChoice = scanner.nextLine();
+
+                    switch (nestedLedgerChoice) {
+                        case "1":
+                            // month to year
+                            break;
+                        case "2":
+                            // previous month
+                            break;
+                        case "3":
+                            // year to date
+                            break;
+                        case "4":
+                            // previous year
+                            break;
+                        case "5":
+                            // search by vendor
+                            break;
+                        case "0":
+                            break;
+                        default:
+                            System.out.println("Invalid option.");
+                    }
+
+                } while(nestedLedgerChoice.equals("0")); // goes back to report page
                 break;
+
             case "H":
                 System.out.println("test4");
                 break;
-
         }
 
 
